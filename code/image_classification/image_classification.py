11c2a57613987a72deeaaa50fc79e1759084e5cf
Kevin Coakley <kcoakley@sdsc.edu>
2024-01-17 21:31:53 -0800

    image_classification 1.0.0
    Signed-off-by: Kevin Coakley <kcoakley@sdsc.edu>

---
+++ code/image_classification/image_classification.py
@@ -0,0 +1,534 @@
+import argparse, csv, os, sys, yaml
+from datetime import datetime
+
+script_version = "1.0.0"
+
+
+def get_dataset_details(dataset_name):
+    """
+    ## Datasets definition dictionary
+    """
+    datasets = {
+        "cifar100": {
+            "name": "cifar100",
+            "split": ["train[:80%]", "train[80%:100%]", "test"],
+            "num_classes": 100,
+            "dataset_shape": (128, 128, 3),
+        },
+        "cifar10": {
+            "name": "cifar10",
+            "split": ["train[:80%]", "train[80%:100%]", "test"],
+            "num_classes": 10,
+            "dataset_shape": (128, 128, 3),
+        },
+        "imagenette": {
+            "name": "imagenette/320px-v2",
+            "split": ["train[:80%]", "train[80%:100%]", "validation"],
+            "num_classes": 10,
+            "dataset_shape": (224, 224, 3),
+        },
+        "oxford_iiit_pet": {
+            "name": "oxford_iiit_pet",
+            "split": ["train[:80%]", "train[80%:100%]", "test"],
+            "num_classes": 37,
+            "dataset_shape": (299, 299, 3),
+        },
+        "oxford_flowers102": {
+            "name": "oxford_flowers102",
+            "split": ["train", "validation", "test"],
+            "num_classes": 102,
+            "dataset_shape": (299, 299, 3),
+        },
+        "stanford_dogs": {
+            "name": "stanford_dogs",
+            "split": ["train[:80%]", "train[80%:100%]", "test"],
+            "num_classes": 120,
+            "dataset_shape": (299, 299, 3),
+        },
+    }
+
+    return datasets[dataset_name]
+
+
+def image_classification(
+    machine_learning_framework="TensorFlow",
+    model_name="ResNet20",
+    dataset_name="cifar10",
+    op_determinism=False,
+    batch_size=128,
+    learning_rate=0.001,
+    end_to_end_learning_rate=0.0,
+    lr_scheduler=False,
+    epochs=200,
+    end_to_end_epochs=0,
+    seed_val=1,
+    transfer_learning=False,
+    run_name="",
+    save_model=False,
+    save_predictions=False,
+    save_epoch_logs=False,
+    save_tensorboard_logs=False,
+):
+    if machine_learning_framework == "TensorFlow":
+        from tensorflow_framework import Tensorflow
+
+        framework = Tensorflow()
+        framework.epochs = epochs
+        framework.end_to_end_epochs = end_to_end_epochs
+        framework.learning_rate = learning_rate
+        framework.end_to_end_learning_rate = end_to_end_learning_rate
+        framework.lr_scheduler = lr_scheduler
+        framework.transfer_learning = transfer_learning
+
+        framework.save_epoch_logs = save_epoch_logs
+        framework.save_tensorboard_logs = save_tensorboard_logs
+
+    if seed_val != 1:
+        """
+        ## Configure framework for fixed seed runs
+        """
+        framework.set_random_seed(seed_val)
+
+    if op_determinism:
+        """
+        ## Configure framework deterministic operations
+        """
+        framework.set_op_determinism()
+
+    """
+    ## Get the dataset details
+    """
+    dataset_details = get_dataset_details(dataset_name)
+
+    """
+    ## Load the dataset
+    """
+    # Always use the same random seed for the dataset
+    train_dataset, val_dataset, test_dataset = framework.load_dataset(
+        dataset_details, batch_size
+    )
+
+    """
+    ## Create the model
+    """
+    model = framework.load_model(model_name, dataset_details)
+
+    """
+    ## Create the base name for the log and model files
+    """
+    base_name = os.path.basename(sys.argv[0]).split(".")[0]
+
+    if len(run_name) >= 1:
+        base_name = run_name
+
+    """
+    ## Train the model
+    """
+    # Time the training
+    start_time = datetime.now()
+
+    if seed_val != 1:
+        csv_train_log_file = "%s_log_%s_%s.csv" % (
+            base_name,
+            machine_learning_framework,
+            seed_val,
+        )
+    else:
+        csv_train_log_file = "%s_log_%s_%s.csv" % (
+            base_name,
+            machine_learning_framework,
+            datetime.now().strftime("%Y%m%d%H%M%S"),
+        )
+
+    # Train the model
+    trained_model = framework.train(
+        model,
+        train_dataset,
+        val_dataset,
+        epochs,
+        csv_train_log_file,
+        run_name,
+    )
+
+    # Calculate the training time
+    end_time = datetime.now()
+    training_time = end_time - start_time
+
+    """
+    ## Evaluate the trained model and save the predictions
+    """
+    prediction_path = ""
+
+    if save_predictions:
+        if os.path.exists("predictions/") == False:
+            os.mkdir("predictions/")
+
+        prediction_path = "predictions/"
+
+        if run_name != "":
+            if os.path.exists("predictions/" + run_name + "/") == False:
+                os.mkdir("predictions/" + run_name + "/")
+            prediction_path = "predictions/" + run_name + "/"
+
+    predictions_csv_file = (
+        prediction_path + base_name + "_seed_" + str(seed_val) + ".csv"
+    )
+
+    score = framework.evaluate(
+        trained_model, test_dataset, save_predictions, predictions_csv_file
+    )
+
+    print("Test loss: ", score[0])
+    print("Test accuracy: ", score[1])
+    print("Training time: ", training_time)
+
+    """
+    ## Save the model
+    """
+    if save_model:
+        if os.path.exists("models/") == False:
+            os.mkdir("models/")
+
+        model_path = "models/"
+
+        if run_name != "":
+            if os.path.exists("models/" + run_name + "/") == False:
+                os.mkdir("models/" + run_name + "/")
+            model_path = "models/" + run_name + "/"
+
+        if seed_val != 1:
+            model_path = model_path + base_name + "_seed_" + str(seed_val)
+        else:
+            model_path = (
+                model_path
+                + base_name
+                + "_seed_"
+                + datetime.now().strftime("%Y%m%d%H%M%S")
+            )
+
+        # Append the file extension based on the machine learning framework
+        if machine_learning_framework == "TensorFlow":
+            model_path = model_path + ".h5"
+
+        framework.save(trained_model, model_path)
+
+    return score[0], score[1], training_time
+
+
+def get_system_info(filename):
+    if os.path.exists("system_info.py"):
+        import system_info
+
+        sysinfo = system_info.get_system_info()
+
+        with open("%s.yaml" % filename, "w") as system_info_file:
+            yaml.dump(sysinfo, system_info_file, default_flow_style=False)
+
+        return sysinfo
+    else:
+        return None
+
+
+def save_score(
+    test_loss,
+    test_accuracy,
+    machine_learning_framework,
+    batch_size,
+    learning_rate,
+    end_to_end_learning_rate,
+    lr_scheduler,
+    epochs,
+    end_to_end_epochs,
+    training_time,
+    model_name,
+    dataset_name,
+    op_determinism,
+    seed_val,
+    transfer_learning,
+    filename,
+    run_name="",
+):
+    if machine_learning_framework == "TensorFlow":
+        from tensorflow_framework import Tensorflow
+
+        framework = Tensorflow()
+
+    csv_file = filename + ".csv"
+    write_header = False
+
+    # If determistic is false and the seed value is 1 then the
+    # seed value is totally random and we don't know what it is.
+    if seed_val == 1:
+        seed_val = "random"
+
+    if not os.path.isfile(csv_file):
+        write_header = True
+
+    with open(csv_file, "a") as csvfile:
+        fieldnames = [
+            "run_name",
+            "script_version",
+            "date_time",
+            "fit_time",
+            "python_version",
+            "machine_learning_framework",
+            "framework_version",
+            "batch_size",
+            "learning_rate",
+            "end_to_end_learning_rate",
+            "lr_scheduler",
+            "epochs",
+            "end_to_end_epochs",
+            "model_name",
+            "dataset_name",
+            "random_seed",
+            "op_determinism",
+            "transfer_learning",
+            "test_loss",
+            "test_accuracy",
+        ]
+
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        if write_header:
+            writer.writeheader()
+
+        writer.writerow(
+            {
+                "run_name": run_name,
+                "script_version": script_version,
+                "date_time": datetime.now(),
+                "fit_time": training_time,
+                "python_version": sys.version.replace("\n", ""),
+                "machine_learning_framework": machine_learning_framework,
+                "framework_version": framework.version,
+                "batch_size": batch_size,
+                "learning_rate": learning_rate,
+                "end_to_end_learning_rate": end_to_end_learning_rate,
+                "lr_scheduler": lr_scheduler,
+                "epochs": epochs,
+                "end_to_end_epochs": end_to_end_epochs,
+                "model_name": model_name,
+                "dataset_name": dataset_name,
+                "random_seed": seed_val,
+                "op_determinism": op_determinism,
+                "transfer_learning": transfer_learning,
+                "test_loss": test_loss,
+                "test_accuracy": test_accuracy,
+            }
+        )
+
+
+def parse_arguments(args):
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--op-determinism",
+        dest="op_determinism",
+        help="Run with deterministic operations",
+        action="store_true",
+    )
+
+    parser.add_argument(
+        "--seed-val", dest="seed_val", help="Set the seed value", type=int, default=1
+    )
+
+    parser.add_argument(
+        "--batch-size",
+        dest="batch_size",
+        help="Size of the mini-batches",
+        type=int,
+        default=128,
+    )
+
+    parser.add_argument(
+        "--learning-rate",
+        dest="learning_rate",
+        help="Base learning rate",
+        type=float,
+        default=0.001,
+    )
+
+    parser.add_argument(
+        "--end-to-end-learning-rate",
+        dest="end_to_end_learning_rate",
+        help="end to end training learning rate",
+        type=float,
+        default=0.0,
+    )
+
+    parser.add_argument(
+        "--lr-scheduler",
+        dest="lr_scheduler",
+        help="Use the learning rate scheduler",
+        action="store_true",
+    )
+
+    parser.add_argument(
+        "--epochs",
+        dest="epochs",
+        help="Number of epochs",
+        type=int,
+        default=0,
+    )
+
+    parser.add_argument(
+        "--end-to-end-epochs",
+        dest="end_to_end_epochs",
+        help="Number of epochs for end to end training",
+        type=int,
+        default=0,
+    )
+
+    parser.add_argument(
+        "--run-name",
+        dest="run_name",
+        help="Name of training run",
+        default="",
+    )
+
+    parser.add_argument(
+        "--save-filename",
+        dest="save_filename",
+        help="filename used to save the results",
+        type=str,
+        default=str(os.path.basename(sys.argv[0]).split(".")[0]),
+    )
+
+    parser.add_argument(
+        "--save-model", dest="save_model", help="Save the model", action="store_true"
+    )
+
+    parser.add_argument(
+        "--save-predictions",
+        dest="save_predictions",
+        help="Save the predictions",
+        action="store_true",
+    )
+
+    parser.add_argument(
+        "--save-epoch-logs",
+        dest="save_epoch_logs",
+        help="Save the accuracy and loss logs for each epoch",
+        action="store_true",
+    )
+
+    parser.add_argument(
+        "--tensorboard",
+        dest="save_tensorboard_logs",
+        help="Save TensorBoard logs",
+        action="store_true",
+    )
+
+    parser.add_argument(
+        "--ml-framework",
+        dest="machine_learning_framework",
+        help="Name of Machine Learning framework",
+        default="TensorFlow",
+        choices=[
+            "TensorFlow",
+        ],
+        required=True,
+    )
+
+    parser.add_argument(
+        "--model-name",
+        dest="model_name",
+        help="Name of model to train",
+        default="Xception",
+        choices=[
+            "DenseNet121",
+            "DenseNet169",
+            "EfficientNetB0",
+            "EfficientNetB3",
+            "ResNet50V2",
+            "ResNet101V2",
+            "Xception",
+        ],
+        required=True,
+    )
+
+    parser.add_argument(
+        "--dataset-name",
+        dest="dataset_name",
+        help="cifar10, cifar100, imagenette, oxford_flowers102, oxford_iiit_pet, stanford_dogs, dtd, caltech101",
+        default="cifar10",
+        choices=[
+            "cifar10",
+            "cifar100",
+            "imagenette",
+            "oxford_iiit_pet",
+            "oxford_flowers102",
+            "stanford_dogs",
+            "dtd",
+            "caltech101",
+        ],
+        required=True,
+    )
+
+    parser.add_argument(
+        "--transfer-learning",
+        dest="transfer_learning",
+        help="Use transfer learning to train the model using the imagenet weights",
+        action="store_true",
+    )
+
+    return parser.parse_args(args)
+
+
+if __name__ == "__main__":
+    args = parse_arguments(sys.argv[1:])
+
+    save_filename = args.save_filename
+
+    system_info = get_system_info(save_filename)
+    seed_val = args.seed_val
+    epochs = args.epochs
+
+    print(
+        "\nImage Classification (%s - %s - %s): [%s]\n======================\n"
+        % (
+            args.machine_learning_framework,
+            args.model_name,
+            args.dataset_name,
+            seed_val,
+        )
+    )
+    test_loss, test_accuracy, training_time = image_classification(
+        machine_learning_framework=args.machine_learning_framework,
+        model_name=args.model_name,
+        dataset_name=args.dataset_name,
+        op_determinism=args.op_determinism,
+        batch_size=args.batch_size,
+        learning_rate=args.learning_rate,
+        end_to_end_learning_rate=args.end_to_end_learning_rate,
+        lr_scheduler=args.lr_scheduler,
+        epochs=epochs,
+        end_to_end_epochs=args.end_to_end_epochs,
+        seed_val=seed_val,
+        transfer_learning=args.transfer_learning,
+        run_name=args.run_name,
+        save_model=args.save_model,
+        save_predictions=args.save_predictions,
+        save_epoch_logs=args.save_epoch_logs,
+        save_tensorboard_logs=args.save_tensorboard_logs,
+    )
+    save_score(
+        test_loss=test_loss,
+        test_accuracy=test_accuracy,
+        machine_learning_framework=args.machine_learning_framework,
+        batch_size=args.batch_size,
+        learning_rate=args.learning_rate,
+        end_to_end_learning_rate=args.end_to_end_learning_rate,
+        lr_scheduler=args.lr_scheduler,
+        epochs=epochs,
+        end_to_end_epochs=args.end_to_end_epochs,
+        training_time=training_time,
+        model_name=args.model_name,
+        dataset_name=args.dataset_name,
+        op_determinism=args.op_determinism,
+        seed_val=seed_val,
+        transfer_learning=args.transfer_learning,
+        filename=save_filename,
+        run_name=args.run_name,
+    )
---
+++ code/image_classification/tensorflow_framework.py
@@ -0,0 +1,336 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+import csv, datetime, math, random
+import numpy as np
+from sklearn.metrics import accuracy_score
+
+
+class Tensorflow:
+    def __init__(self):
+        self.version = tf.version.VERSION
+        self.epochs = 0
+        self.end_to_end_epochs = 0
+        self.learning_rate = 0.0
+        self.end_to_end_learning_rate = 0.0
+        self.lr_scheduler = False
+        self.transfer_learning = False
+
+        self.save_epoch_logs = False
+        self.save_tensorboard_logs = False
+
+    def set_random_seed(self, seed_val):
+        """
+        ## Configure Tensorflow for fixed seed runs
+        """
+        major, minor, revision = tf.version.VERSION.split(".")
+
+        if int(major) >= 2 and int(minor) >= 7:
+            # Sets all random seeds for the program (Python, NumPy, and TensorFlow).
+            # Supported in TF 2.7.0+
+            tf.keras.utils.set_random_seed(seed_val)
+            print("Setting random seed using tf.keras.utils.set_random_seed()")
+        else:
+            # for TF < 2.7
+            random.seed(seed_val)
+            np.random.seed(seed_val)
+            tf.random.set_seed(seed_val)
+            print("Setting random seeds manually")
+
+    def set_op_determinism(self):
+        """
+        ## Configure Tensorflow for deterministic operations
+        """
+        major, minor, revision = tf.version.VERSION.split(".")
+
+        # Configures TensorFlow ops to run deterministically to enable reproducible
+        # results with GPUs (Supported in TF 2.8.0+)
+        if int(major) >= 2 and int(minor) >= 8:
+            tf.config.experimental.enable_op_determinism()
+            print("Enabled op determinism")
+        else:
+            print("Op determinism not supported")
+            exit()
+
+    def load_dataset(self, dataset_details, batch_size):
+        dataset_name = dataset_details["name"]
+        dataset_split = dataset_details["split"]
+        dataset_shape = dataset_details["dataset_shape"]
+
+        def preprocessing(image, label):
+            image = tf.cast(image, tf.float32)
+            if self.transfer_learning == False:
+                image = tf.image.per_image_standardization(image)
+            image = tf.image.resize(
+                image, dataset_shape[:2], antialias=False, method="nearest"
+            )
+            return image, label
+
+        def augmentation(image, label):
+            image = tf.image.resize_with_crop_or_pad(
+                image,
+                int(dataset_shape[0] + (dataset_shape[0] * 0.2)),
+                int(dataset_shape[1] + (dataset_shape[1] * 0.2)),
+            )
+            image = tf.image.random_crop(image, dataset_shape)
+            image = tf.image.random_flip_left_right(image)
+            return image, label
+
+        train, val, test = tfds.load(
+            dataset_name,
+            split=dataset_split,
+            as_supervised=True,
+        )
+
+        print(f"Number of training samples: {train.cardinality()}")
+        print(f"Number of validation samples: {val.cardinality()}")
+        print(f"Number of test samples: {test.cardinality()}")
+
+        # Batch and prefetch the dataset
+        train_dataset = (
+            train.map(preprocessing)
+            .map(augmentation)
+            .shuffle(1000)
+            .batch(batch_size)
+            .prefetch(tf.data.AUTOTUNE)
+        )
+        val_dataset = (
+            val.map(preprocessing).batch(batch_size).prefetch(tf.data.AUTOTUNE)
+        )
+        test_dataset = (
+            test.map(preprocessing).batch(batch_size).prefetch(tf.data.AUTOTUNE)
+        )
+
+        return train_dataset, val_dataset, test_dataset
+
+    def load_model(self, model_name, dataset_details):
+        num_classes = dataset_details["num_classes"]
+        dataset_shape = dataset_details["dataset_shape"]
+
+        models = {
+            "DenseNet121": {
+                "application": tf.keras.applications.DenseNet121,
+                "preprocess_input": tf.keras.applications.densenet.preprocess_input,
+            },
+            "DenseNet169": {
+                "application": tf.keras.applications.DenseNet169,
+                "preprocess_input": tf.keras.applications.densenet.preprocess_input,
+            },
+            "ResNet50V2": {
+                "application": tf.keras.applications.resnet_v2.ResNet50V2,
+                "preprocess_input": tf.keras.applications.resnet_v2.preprocess_input,
+            },
+            "ResNet101V2": {
+                "application": tf.keras.applications.resnet_v2.ResNet101V2,
+                "preprocess_input": tf.keras.applications.resnet_v2.preprocess_input,
+            },
+            "EfficientNetB0": {
+                "application": tf.keras.applications.EfficientNetB0,
+                "preprocess_input": tf.keras.applications.efficientnet.preprocess_input,
+            },
+            "EfficientNetB3": {
+                "application": tf.keras.applications.EfficientNetB3,
+                "preprocess_input": tf.keras.applications.efficientnet.preprocess_input,
+            },
+            "Xception": {
+                "application": tf.keras.applications.Xception,
+                "preprocess_input": tf.keras.applications.xception.preprocess_input,
+            },
+        }
+
+        random_init_args = {
+            "include_top": True,
+            "weights": None,
+            "input_shape": dataset_shape,
+            "classes": num_classes,
+            "classifier_activation": "softmax",
+        }
+        transfer_init_args = {
+            "include_top": False,
+            "weights": "imagenet",
+            "input_shape": dataset_shape,
+            "pooling": None,
+        }
+
+        if self.transfer_learning == False:
+            base_model = models[model_name]["application"](**random_init_args)
+            return base_model
+
+        else:
+            base_model = models[model_name]["application"](**transfer_init_args)
+
+            # Freeze the base_model
+            base_model.trainable = False
+
+            inputs = tf.keras.Input(shape=dataset_shape)
+
+            # Build the top of the model
+            x = models[model_name]["preprocess_input"](inputs)
+            x = base_model(x)
+
+            x = tf.keras.layers.GlobalAveragePooling2D()(x)
+            x = tf.keras.layers.Dropout(0.2)(x)
+
+            outputs = tf.keras.layers.Dense(num_classes, activation="softmax")(x)
+
+            transfer_model = tf.keras.Model(inputs, outputs)
+
+            return transfer_model
+
+    def train(
+        self,
+        model,
+        train_dataset,
+        val_dataset,
+        epochs,
+        csv_train_log_file=None,
+        run_name="",
+    ):
+        """
+        ## Define the learning rate schedule
+        """
+
+        def lr_schedule(epoch):
+            if epoch < math.ceil(self.epochs * 0.5):
+                return self.learning_rate
+            elif epoch < math.ceil(self.epochs * 0.75):
+                return self.learning_rate * 0.1
+            else:
+                return self.learning_rate * 0.01
+
+        model.summary(show_trainable=True)
+
+        model.compile(
+            optimizer=tf.keras.optimizers.Adam(learning_rate=lr_schedule(0)),
+            loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+            metrics=["accuracy"],
+        )
+
+        # Define the learning rate scheduler callback
+        lr_scheduler = tf.keras.callbacks.LearningRateScheduler(lr_schedule)
+
+        # Define csv logger callback
+        csv_logger = tf.keras.callbacks.CSVLogger(csv_train_log_file)
+
+        # Define tensorboard callback
+        log_dir = (
+            "logs/fit/"
+            + run_name
+            + "-"
+            + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+        )
+        tensorboard_callback = tf.keras.callbacks.TensorBoard(
+            log_dir=log_dir, histogram_freq=1
+        )
+
+        # Define callbacks
+        callbacks = []
+
+        if self.save_epoch_logs:
+            callbacks.append(csv_logger)
+
+        if self.lr_scheduler:
+            callbacks.append(lr_scheduler)
+
+        if self.save_tensorboard_logs:
+            callbacks.append(tensorboard_callback)
+
+        # Train the model
+        model.fit(
+            train_dataset,
+            epochs=epochs,
+            validation_data=val_dataset,
+            callbacks=callbacks,
+        )
+
+        if self.transfer_learning:
+            # Unfreeze the base_model
+            for layer in model.layers:
+                if not isinstance(layer, tf.keras.layers.BatchNormalization):
+                    layer.trainable = True
+
+            model.summary(show_trainable=True)
+
+            model.compile(
+                optimizer=tf.keras.optimizers.Adam(
+                    learning_rate=self.end_to_end_learning_rate
+                ),  # Low learning rate
+                loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+                metrics=["accuracy"],
+            )
+
+            # Define tensorboard callback
+            log_dir = (
+                "logs/fit/"
+                + run_name
+                + "-end-2-end-"
+                + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+            )
+            tensorboard_callback = tf.keras.callbacks.TensorBoard(
+                log_dir=log_dir, histogram_freq=1
+            )
+
+            # Define callbacks
+            callbacks = []
+
+            if self.save_tensorboard_logs:
+                callbacks.append(tensorboard_callback)
+
+            print("Fitting the end-to-end model")
+            model.fit(
+                train_dataset,
+                epochs=self.end_to_end_epochs,
+                validation_data=val_dataset,
+                callbacks=callbacks,
+            )
+
+        return model
+
+    def evaluate(
+        self, model, test_dataset, save_predictions=False, predictions_csv_file=None
+    ):
+        # Get the predictions
+        predictions = model.predict(test_dataset)
+
+        # Get the labels of the validation dataset
+        test_dataset = test_dataset.unbatch()
+        labels = np.asarray(list(test_dataset.map(lambda x, y: y)))
+
+        # Get the index to the highest probability
+        y_true = labels
+        y_pred = np.argmax(predictions, axis=1)
+
+        if save_predictions:
+            # Add the true values to the first column and the predicted values to the second column
+            true_and_pred = np.vstack((y_true, y_pred)).T
+
+            # Add each label predictions to the true and predicted values
+            csv_output_array = np.concatenate((true_and_pred, predictions), axis=1)
+
+            # Save the predictions to a csv file
+            with open(predictions_csv_file, "w") as csvfile:
+                writer = csv.writer(csvfile)
+
+                csv_columns = ["true_value", "predicted_value"]
+                for i in range(predictions.shape[1]):
+                    csv_columns.append("label_" + str(i))
+
+                writer.writerow(csv_columns)
+                writer.writerows(csv_output_array.tolist())
+
+        # Calucate the validation loss
+        loss = tf.keras.losses.SparseCategoricalCrossentropy()
+        validation_loss = loss(labels, predictions).numpy()
+
+        # Use sklearn to calculate the validation accuracy
+        validation_accuracy = accuracy_score(y_true, y_pred)
+
+        return [validation_loss, validation_accuracy]
+
+    def save(self, model, model_path):
+        model.save(model_path)
+
+    def load(self, model_path):
+        model = tf.keras.models.load_model(model_path)
+
+        return model
